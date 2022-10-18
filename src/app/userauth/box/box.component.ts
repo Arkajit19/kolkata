026b4f619da9c3677e2a3dfb5bf25cbdcc59c624
Arkajit <59366904+Arkajit19@users.noreply.github.com>
2022-10-18 18:41:57 +0530

    todaynewchanges one

---
+++ src/app/userauth/box/box.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-box',
+  templateUrl: './box.component.html',
+  styleUrls: ['./box.component.css'],
+})
+export class BoxComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+  isValid: boolean = true;
+  Changecommand(val: boolean) {
+    
+    this.isValid = val;
+  }
+}
--- src/app/userauth/userauth.module.ts
+++ src/app/userauth/userauth.module.ts
@@ -4,16 +4,10 @@ import { HomeComponent } from './home/home.component';
 import { MatButtonModule } from '@angular/material/button';
 import { FormsModule } from '@angular/forms';
 import {ReactiveFormsModule} from '@angular/forms';
+import { BoxComponent } from './box/box.component';
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [
-    CommonModule,
-    MatButtonModule,
-    FormsModule,
-    ReactiveFormsModule
-  ],
-  exports: [HomeComponent],
+  declarations: [HomeComponent, BoxComponent],
+  imports: [CommonModule, MatButtonModule, FormsModule, ReactiveFormsModule],
+  exports: [HomeComponent, BoxComponent],
 })
-export class UserauthModule { }
+export class UserauthModule {}
